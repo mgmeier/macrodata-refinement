0f90d1d68d97e1caf040dcd2dbc1fb69dc40639b
Michael Karg <mgoremeier@gmail.com>
2025-02-07 21:21:29 +0100

    Text based rendering; height styles

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
 import Control.Concurrent
 import Control.DeepSeq (deepseq)
 import Control.Exception
@@ -30,25 +34,78 @@ runMDR size@(xSize, ySize) = do
 
   bracket_ useAlternateScreenBuffer (useNormalScreenBuffer >> restoreStdout) $ do
     frameBuffer <- newEmptyMVar
-    setSGR [SetConsoleIntensity FaintIntensity, SetColor Foreground Dull Cyan]
+    setSGR (heightStyle 0)
     initRenderer fps frameBuffer
     createFrames size fps frameBuffer
   where
-    fps        = 2
+    fps        = 16
     bufferSize = head $ tail $ dropWhile (< xSize * ySize) $ iterate (* 2) 4096
 
-createFrames :: (Int, Int) -> Int -> MVar String -> IO ()
-createFrames (xSize, ySize) _fps frameBuffer = forever $ do
-  buf <- replicateM (xSize * ySize) $ (['0' .. '9'] !!) <$> randomRIO (0, 9)
-  buf `deepseq` putMVar frameBuffer buf
+data Counters = Counters
+      { xPos :: {-# UNPACK #-} !Double
+      , yPos :: {-# UNPACK #-} !Double
+      }
 
-initRenderer :: Int -> MVar String -> IO ()
+newtype Height = Height Text
+
+createFrames :: (Int, Int) -> Int -> MVar Text -> IO ()
+createFrames res@(ySize, xSize) _fps frameBuffer = do
+  source <- T.pack <$> replicateM (xSize * ySize)
+              ((['0' .. '9'] !!) <$> randomRIO (0, 9))
+  let
+    frameLines = T.chunksOf xSize source
+
+    go counters@Counters{..} = do
+      let
+        x     = round (xMax + (xMax * sin xPos))
+        hs    = [(x, height 1), (10, height 2), (10, height 3), (10, height 2), (10, height 1), (10, height 0)]
+        frame = T.concat $ map (insertHeights hs) frameLines
+      frame `deepseq` putMVar frameBuffer frame
+      go $ stepCounters step1 counters
+
+  writeFile "debug.log" (show (T.length $ head frameLines) ++ "x" ++ show (length frameLines) ++ "\n" ++ show res ++ "\n")
+  go startCounters
+  where
+    step1  = 0.8 / fromIntegral _fps
+    height = Height . T.pack . setSGRCode . heightStyle
+
+    xLen   = 50 -- xSize `div` 10
+    xMax   = fromIntegral (xSize - xLen) / 2
+    -- yLen   = fromIntegral ySize / 20
+
+
+startCounters :: Counters
+startCounters = Counters 1.0 1.0
+
+{-# INLINE stepCounters #-}
+stepCounters :: Double -> Counters -> Counters
+stepCounters s1 Counters{..} =
+  Counters (step s1 xPos) yPos
+  where
+    phase    = 2 * pi
+    step s c = let c' = c - s in if c' < 0.0 then c' + phase else c'
+
+insertHeights :: [(Int, Height)] -> Text -> Text
+insertHeights [] t = t
+insertHeights ((ofs, Height h):rest) t =
+  let (pref, suff) = T.splitAt ofs t
+  in pref <> h <> insertHeights rest suff
+
+
+initRenderer :: Int -> MVar Text -> IO ()
 initRenderer fps frameBuffer =
   void $ forkIO $ forever $ do
     out <- takeMVar frameBuffer
-    hPutStr stdout $ resetScreen <> out
+    T.hPutStr stdout $ resetScreen <> out
     hFlush stdout
     threadDelay delay
   where
-    resetScreen = clearScreenCode <> setCursorPositionCode 0 0
+    resetScreen = T.pack $ clearScreenCode ++ setCursorPositionCode 0 0
     delay       = 1000 * (1000 `div` fps)
+
+heightStyle :: Int -> [SGR]
+heightStyle 0 = [SetConsoleIntensity FaintIntensity,  SetColor Foreground Dull Cyan]
+heightStyle 1 = [SetConsoleIntensity NormalIntensity, SetColor Foreground Dull Cyan]
+heightStyle 2 = [SetConsoleIntensity NormalIntensity, SetColor Foreground Vivid Cyan]
+heightStyle 3 = [SetConsoleIntensity BoldIntensity,   SetColor Foreground Vivid Cyan]
+heightStyle n = error $ "heightStyle: " ++ show n ++ "; expected [0-3]"
